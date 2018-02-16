30316c29b0dccffb39fe410cc1d94ddfeb4c6cd1
HwangboHyub <hwangbohyub@gmail.com>
2018-02-17 01:53:39 +0900

    fix_read_me

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
+  config.fog_provider = 'fog/aws'                       # required
   config.fog_credentials = {
-    provider:              'AWS',                        # required
+    provider:              'AWS',                      # required
     aws_access_key_id:     '',                        # required
     aws_secret_access_key: '',                        # required
     region:                'ap-northeast-2',                  # optional, defaults to 'us-east-1'
